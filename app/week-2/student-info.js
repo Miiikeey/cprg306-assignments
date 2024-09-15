0994a4a68427199785f7c6d1690048d7f89e338b
Mike Cabaluna <mike.cabaluna@edu.sait.ca>
2024-09-15 00:55:10 -0600

    Week-2
    Created week-2 page, added texts of  shopping list, name, and git hub link

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+  return (
+    <div> 
+        <p>Mike Cabaluna</p>
+
+        <ul>
+            <li>
+                <Link href="https://github.com/Miiikeey?tab=repositories">https://github.com</Link>
+            </li>
+        </ul>
+    </div>
+
+    )
+}
